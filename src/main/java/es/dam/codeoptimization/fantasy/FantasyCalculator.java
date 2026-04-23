d0be46f5382cf702e5872f1c21140fbfa48e5036
Xabi <xabi.gg06@gmail.com>
2026-04-23 10:49:22 +0200

    Commit Optimizacion de Codigo

--- src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
+++ src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
@@ -13,132 +13,132 @@ public class FantasyCalculator {
 
     // Method to calculate the points
     public static int calcP(PlayerStats s) {
-        int r = 0; 
+        int total = 0; 
         int punts = 0;
         
         int minutes = s.minutes;
-        int g = s.goals;
-        int a = s.assists;
-        boolean y = s.yellowCard;
-        boolean red = s.redCard;
-        int p = s.saves;
-        int ga = s.goalsAgainst;
-        char res = s.matchResult;
-        String pos = s.position;
+        int goals = s.goals;
+        int assits = s.assists;
+        boolean yellowCard = s.yellowCard;
+        boolean redCard = s.redCard;
+        int saves = s.saves;
+        int goalsAgainst = s.goalsAgainst;
+        char result = s.matchResult;
+        String positionString = s.position;
 
         // --- GOALKEEPER LOGIC ---
-        if (pos.equals("PORTERO")) {
-            if (m > 0 && m < 60) {
-                r = r + 3;
-            } else if (m >= 60) {
-                r = r + 5;
+        if (positionString.equals("PORTERO")) {
+            if (minutes > 0 && minutes < 60) {
+                total = total + 3;
+            } else if (minutes >= 60) {
+                total = total + 5;
             }
 
-            for (int i = 0; i < g; i++) {
-                r = r + 5;
+            for (int i = 0; i < goals; i++) {
+                total = total + 5;
             }
 
-            r = r + (a * 6);
+            total = total + (assits * 6);
 
             // 1 point per save
-            r = r + p; 
+            total = total + saves; 
             
-            if (ga == 0) {
-                r = r + 5; 
-            } else if (ga == 1) {
-                r = r + 3;
-            } else if (ga == 2) {
-                r = r + 1;
+            if (goalsAgainst == 0) {
+                total = total + 5; 
+            } else if (goalsAgainst == 1) {
+                total = total + 3;
+            } else if (goalsAgainst == 2) {
+                total = total + 1;
             }
 
-            if (y == true) r = r - 3; 
-            if (red == true) r = r - 5;
+            if (yellowCard == true) total = total - 3; 
+            if (redCard == true) total = total - 5;
             
-            if (res == 'G') {
-                r = r + 5;
-            } else if (res == 'E') {
-                r = r + 2;
+            if (result == 'G') {
+                total = total + 5;
+            } else if (result == 'E') {
+                total = total + 2;
             }
 
         // --- DEFENDER LOGIC ---
-        } else if (pos.equals("DEFENSA")) {
-            if (m > 0 && m < 60) {
-                r = r + 3;
-            } else if (m >= 60) {
-                r = r + 5;
+        } else if (positionString.equals("DEFENSA")) {
+            if (minutes > 0 && minutes < 60) {
+                total = total + 3;
+            } else if (minutes >= 60) {
+                total = total + 5;
             }
 
-            for (int i = 0; i < g; i++) {
-                r = r + 5;
+            for (int i = 0; i < goals; i++) {
+                total = total + 5;
             }
 
-            r = r + (a * 6);
+            total = total + (assits * 6);
 
-            if (ga == 0) {
-                r = r + 5; 
-            } else if (ga == 1) {
-                r = r + 3;
-            } else if (ga == 2) {
-                r = r + 1;
+            if (goalsAgainst == 0) {
+                total = total + 5; 
+            } else if (goalsAgainst == 1) {
+                total = total + 3;
+            } else if (goalsAgainst == 2) {
+                total = total + 1;
             }
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yellowCard == true) total = total - 3;
+            if (redCard == true) total = total - 5;
             
-            if (res == 'G') {
-                r = r + 5;
-            } else if (res == 'E') {
-                r = r + 2;
+            if (result == 'G') {
+                total = total + 5;
+            } else if (result == 'E') {
+                total = total + 2;
             }
 
         // --- MIDFIELDER LOGIC ---
-        } else if (pos.equals("MEDIO")) {
-            if (m > 0 && m < 60) {
-                r = r + 3;
-            } else if (m >= 60) {
-                r = r + 5;
+        } else if (positionString.equals("MEDIO")) {
+            if (minutes > 0 && minutes < 60) {
+                total = total + 3;
+            } else if (minutes >= 60) {
+                total = total + 5;
             }
 
-            for (int i = 0; i < g; i++) {
-                r = r + 5;
+            for (int i = 0; i < goals; i++) {
+                total = total + 5;
             }
 
-            r = r + (a * 6);
+            total = total + (assits * 6);
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yellowCard == true) total = total - 3;
+            if (redCard == true) total = total - 5;
             
-            if (res == 'G') {
-                r = r + 5;
-            } else if (res == 'E') {
-                r = r + 2;
+            if (result == 'G') {
+                total = total + 5;
+            } else if (result == 'E') {
+                total = total + 2;
             }
 
         // --- FORWARD LOGIC ---
-        } else if (pos.equals("DELANTERO")) {
-            if (m > 0 && m < 60) {
-                r = r + 3;
-            } else if (m >= 60) {
-                r = r + 5;
+        } else if (positionString.equals("DELANTERO")) {
+            if (minutes > 0 && minutes < 60) {
+                total = total + 3;
+            } else if (minutes >= 60) {
+                total = total + 5;
             }
 
-            for (int i = 0; i < g; i++) {
-                r = r + 6;
+            for (int i = 0; i < goals; i++) {
+                total = total + 6;
             }
 
-            r = r + (a * 5);
+            total = total + (assits * 5);
 
-            if (y == true) r = r - 3;
-            if (red == true) r = r - 5;
+            if (yellowCard == true) total = total - 3;
+            if (redCard == true) total = total - 5;
             
-            if (res == 'G') {
-                r = r + 5;
-            } else if (res == 'E') {
-                r = r + 2;
+            if (result == 'G') {
+                total = total + 5;
+            } else if (result == 'E') {
+                total = total + 2;
             }
         }
 
-        return r;
+        return total;
     }
 }
 // Commit Prueba
